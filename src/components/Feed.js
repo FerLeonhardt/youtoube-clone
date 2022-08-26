e4be4152dd0404f1082162d5c31f01c01f08c4e7
FerLeonhardt <fnleonhardt@gmail.com>
2022-08-26 19:27:14 -0400

    something

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,6 @@
 import { Box, Stack, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import SideBar from './SideBar';
+import {SideBar,Videos} from './index';
 
 const Feed = () => {
   //const [first, setfirst] = useState(second)
@@ -17,13 +17,16 @@ const Feed = () => {
         }}
       >
         <SideBar />
-        <Typography
-          className='copyright'
-          variant='body2'
-          sx={{ mt: 1.5, color: '#fff' }}
-        >
-          copyright youtube clone ferchu
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 GauchoDev
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          New <span style={{color:'#F31503'}}>
+          Videos</span>
+        </Typography>
+            <Videos videos={[]} />
       </Box>
     </Stack>
   );
