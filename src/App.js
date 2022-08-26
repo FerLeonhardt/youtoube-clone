9d11ecd4743d4b22aaf8a7ccfcdfc6e17de63fc4
FerLeonhardt <fnleonhardt@gmail.com>
2022-08-26 16:33:52 -0400

    layout and routes settle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 const app = () => {
   return (
-    <div>hello world</div>
-  )
-}
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/video/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
-export default app
+export default app;
